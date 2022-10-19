a25827c26282bc8a5cd2eee8190062364b310c61
anveloper <hitedin@gmail.com>
2022-10-20 06:19:35 +0900

    Feat: 네비게이션 기능 추가

---
+++ android/app/src/main/java/com/youme/naya/MainScreen.kt
@@ -0,0 +1,12 @@
+package com.youme.naya
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+//    val navController = rememberNavController()
+    Scaffold(bottomBar = {}) {
+
+    }
+}
