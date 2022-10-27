2d6784f3e2483e878cdd3379467f1f49311938de
김성찬 <kimsc0714@naver.com>
2022-10-27 17:10:25 +0900

    ✨ feat: nUYA 카드 조회 화면 내 카드 리스트 UI 구현

--- android/app/src/main/java/com/youme/naya/screens/NuyaCardHolder.kt
+++ android/app/src/main/java/com/youme/naya/screens/NuyaCardHolder.kt
@@ -14,11 +14,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -32,18 +28,21 @@ import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidViewBinding
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.youme.naya.R
+import com.youme.naya.card.CustomCardStackView
+import com.youme.naya.card.TestStackAdapter
+import com.youme.naya.databinding.CardStackViewMainBinding
 import com.youme.naya.ui.theme.*
 
 enum class MultiFabState {
@@ -81,7 +80,8 @@ fun NuyaCardHolderScreen(navController: NavHostController) {
         SearchInput()
         NayaBcardSwitchButtons()
         Box(Modifier.fillMaxSize()) {
-            CardList()
+//            CardList()
+            CustomCardStackView()
             MultiFloatingActionButton(
                 listOf(
                     MultiFabItem(
