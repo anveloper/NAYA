8cb6e045ea01e9b01040ce7103c5b6f1212f0b05
anveloper <hitedin@gmail.com>
2022-11-02 13:46:36 +0900

    feat: 이미지 컴포즈 수정중

--- android/app/src/main/java/com/youme/naya/camera/ImageCompose.kt
+++ android/app/src/main/java/com/youme/naya/camera/ImageCompose.kt
@@ -1,17 +1,12 @@
 package com.youme.naya.camera
 
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.res.painterResource
-import coil.compose.rememberImagePainter
-import java.net.URI
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ImageCompose(bitmap: Bitmap) {
@@ -21,4 +16,16 @@ fun ImageCompose(bitmap: Bitmap) {
         modifier = Modifier.fillMaxSize()
     )
 
+}
+
+
+@Preview(
+    name = "Capture Image",
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun CaptureImagePreview() {
+    val bitmap = Bitmap.createBitmap(200, 360, Bitmap.Config.ARGB_8888)
+    ImageCompose(bitmap)
 }
