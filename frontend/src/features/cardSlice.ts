b6157570946817be555a8ac1fc1f35575e0ef2c4
anveloper <hitedin@gmail.com>
2022-11-07 23:56:38 +0900

    feat: 임시 카드 뷰 페이지 구현

--- frontend/src/features/cardSlice.ts
+++ frontend/src/features/cardSlice.ts
@@ -1,28 +1,28 @@
+import { RootState } from "./../app/store";
+import { client } from "./../app/Axios";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import Axios from "axios";
 
-interface CardConfig {
-  card: {
-    title: String;
-    name: String;
-  };
+export interface RequestParams {
+  userId: string;
+  cardId: number;
+}
 
+export interface CardConfig {
+  imageUrl: string;
   status: "idle" | "loading" | "success" | "failed";
 }
 
 const initialState: CardConfig = {
-  card: {
-    title: "",
-    name: "",
-  },
+  imageUrl:
+    "https://firebasestorage.googleapis.com/v0/b/naya-365407.appspot.com/o/naya%2FOxkibXM9LhYHtBAoLYaB7nQnF4s1%2FNAYA-MEDIA-1667740225913.png?alt=media&token=40dbfebb-3ce4-4872-8b9b-068723c12d62",
   status: "idle",
 };
 
 export const getCardInfo = createAsyncThunk(
   "card/getCardInfo",
-  async (params: String, { rejectWithValue }) => {
+  async (params: RequestParams, { rejectWithValue }) => {
     try {
-      const response = await Axios.get("/", { params });
+      const response = await client.get("/", { params });
       return response.data;
     } catch (err) {
       return rejectWithValue(err);
@@ -41,7 +41,7 @@ const CardSlice = createSlice({
       })
       .addCase(getCardInfo.fulfilled, (state, { payload }) => {
         state.status = "success";
-        state.card = payload;
+        state.imageUrl = payload;
       })
       .addCase(getCardInfo.rejected, (state) => {
         state.status = "failed";
@@ -50,3 +50,5 @@ const CardSlice = createSlice({
 });
 
 export default CardSlice.reducer;
+
+export const selectImageUrl = (state: RootState) => state.card.imageUrl;
