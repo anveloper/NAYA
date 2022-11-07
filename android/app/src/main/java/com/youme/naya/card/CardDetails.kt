1a9202c38a3daa9f242923cfb4aefec47b73f8ac
김성찬 <kimsc0714@naver.com>
2022-11-07 17:54:40 +0900

    ♻️ refactor: 삭제 모달 창 컴포저블 분리

--- android/app/src/main/java/com/youme/naya/card/CardDetails.kt
+++ android/app/src/main/java/com/youme/naya/card/CardDetails.kt
@@ -2,12 +2,9 @@ package com.youme.naya.card
 
 import android.app.Activity
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.AlertDialog
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -15,12 +12,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.squareup.moshi.Moshi
@@ -30,8 +25,6 @@ import com.youme.naya.components.OutlinedSmallButton
 import com.youme.naya.components.PrimarySmallButton
 import com.youme.naya.database.entity.Card
 import com.youme.naya.database.viewModel.CardViewModel
-import com.youme.naya.ui.theme.NeutralMetal
-import com.youme.naya.ui.theme.PrimaryDark
 import com.youme.naya.ui.theme.fonts
 
 @Composable
@@ -137,52 +130,4 @@ fun CardDetailsItem(
         Text(text = fieldName, fontFamily = fonts, fontWeight = FontWeight.Bold)
         Text(text = fieldValue, fontFamily = fonts, textAlign = TextAlign.End)
     }
-}
-
-@Composable
-fun DeleteAlertDialog(
-    onDelete: () -> Unit,
-    onCancel: () -> Unit
-) {
-    AlertDialog(
-        onDismissRequest = { onCancel() },
-        buttons = {
-            Box(
-                Modifier.padding(24.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(24.dp)
-                ) {
-                    Column(
-                        Modifier.fillMaxWidth(),
-                        verticalArrangement = Arrangement.spacedBy(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            text = "명함을 삭제하시겠어요?",
-                            fontFamily = fonts,
-                            fontWeight = FontWeight.Bold,
-                            color = PrimaryDark,
-                            fontSize = 16.sp,
-                            textAlign = TextAlign.Center
-                        )
-                        Text(
-                            text = "한 번 삭제한 명함은 되돌릴 수 없으니 신중하게 선택하세요",
-                            fontFamily = fonts,
-                            color = NeutralMetal,
-                            fontSize = 12.sp,
-                            textAlign = TextAlign.Center
-                        )
-                    }
-                    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-                        OutlinedSmallButton(text = "삭제하기") { onDelete() }
-                        PrimarySmallButton(text = "돌아가기") { onCancel() }
-                    }
-                }
-            }
-        },
-        backgroundColor = Color.White,
-        shape = RoundedCornerShape(16.dp)
-    )
 }
