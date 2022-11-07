0853532f4a722a91f527cd99a12ebece3788694d
hyeryung <hyeryungjoo@gmail.com>
2022-11-07 10:25:41 +0900

    Chore:코드 정리 및 DB 구조 수정

--- backend/NAYA/src/main/java/com/naya/naya/entity/User.java
+++ backend/NAYA/src/main/java/com/naya/naya/entity/User.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "userId")
     private Long userId;
     @Column(name = "email")
