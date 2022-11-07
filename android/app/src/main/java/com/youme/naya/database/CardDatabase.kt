2ef9bbdd15e024f847c99829dde7d1e55bf8a607
Yuna Jin <dbsdk100@gmail.com>
2022-11-07 09:54:12 +0900

    🐛 fix: dB version 수정

--- android/app/src/main/java/com/youme/naya/database/CardDatabase.kt
+++ android/app/src/main/java/com/youme/naya/database/CardDatabase.kt
@@ -10,7 +10,7 @@ import com.youme.naya.database.entity.Schedule
 
 @Database(
     entities = [Card::class, Schedule::class, Member::class],
-    version = 4,
+    version = 3,
     exportSchema = false
 )
 abstract class CardDatabase: RoomDatabase() {
