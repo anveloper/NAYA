074e34dc1a7fab0cdac4cb40a8841638e99e912b
김성찬 <kimsc0714@naver.com>
2022-11-08 17:58:17 +0900

    ✨ feat: 메인 화면 명함 리스트 구현 임시 저장

--- android/app/src/main/java/com/youme/naya/widgets/home/MyBCardList.kt
+++ android/app/src/main/java/com/youme/naya/widgets/home/MyBCardList.kt
@@ -1,9 +1,66 @@
 package com.youme.naya.widgets.home
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.view.View
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidViewBinding
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.youme.naya.R
+import com.youme.naya.database.viewModel.CardViewModel
+import com.youme.naya.databinding.BusinessCardItemBinding
+
 
 @Composable
-fun MyBCardList() {
+fun MyBCardList(context: Context) {
+    val cardViewModel: CardViewModel = hiltViewModel()
+    val businessCards = cardViewModel.businessCardList.collectAsState().value
+
+    AndroidViewBinding(
+        { inflater, parent, _ ->
+            BusinessCardItemBinding.inflate(inflater)
+//            BusinessCardItemBinding.inflate(inflater, parent)
+        },
+        Modifier.fillMaxSize()
+    ) {
+        businessCards.forEach { card ->
+            this.bcardName.text = card.name
+            this.bcardEnglishName.text = card.engName
+            this.bcardCompany.text = card.company
+            this.bcardTeamAndRole.text = card.team + " | " + card.role
+            this.bcardAddress.text = card.address
+            this.bcardMobile.text = card.mobile
+            this.bcardEmail.text = card.email
+            this.bcardLogo.text = "로고 이미지"
+            this.bcardQrcode.text = "QR코드 이미지"
+        }
+
+//        val mCardStackAdapter = CardStackAdapter(context, launcher);
+//        stackviewMain.setAdapter(mCardStackAdapter)
+//        mCardStackAdapter.updateData(businessCards)
+    }
 
 
+
+//    val inflatedFrame: View = getLayoutInflater().inflate(R.layout.demo_text, null)
+//    val frameLayout: FrameLayout = inflatedFrame.findViewById<View>(R.id.screen) as FrameLayout
+//    frameLayout.setDrawingCacheEnabled(true)
+//    frameLayout.measure(
+//        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+//        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+//    )
+//    frameLayout.layout(0, 0, frameLayout.getMeasuredWidth(), frameLayout.getMeasuredHeight())
+//    frameLayout.buildDrawingCache(true)
+//    val bm: Bitmap = frameLayout.getDrawingCache()
+
+
+
+//    val viewModel = viewModel<CardListViewModel>()
+//    viewModel.fetchCards()
+//    val cardList = viewModel.cardUris.value
+//
+//    CardHorizontalList(context, cardList)
 }
--- android/app/src/main/java/com/youme/naya/widgets/home/MyNayaCardList.kt
+++ android/app/src/main/java/com/youme/naya/widgets/home/MyNayaCardList.kt
@@ -1,136 +1,15 @@
 package com.youme.naya.widgets.home
 
-import android.app.Activity
-import android.app.Application
-import android.content.ContentUris
-import android.net.Uri
-import android.provider.MediaStore
-import android.util.DisplayMetrics
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowLeft
-import androidx.compose.material.icons.filled.ArrowRight
-import androidx.compose.material.icons.outlined.Add
+import android.content.Context
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.youme.naya.widgets.items.CardItem
-import com.youme.naya.widgets.items.CardItemPlus
-import kotlinx.coroutines.launch
-
-private val NayaCardContainerModifier = Modifier
-    .fillMaxSize()
-private val NayaCardListModifier = Modifier
-    .fillMaxWidth()
 
 
 @Composable
-fun MyNayaCardList() {
-    // 처음 아이템의 padding을 정해주기 위한 식
-    val context = LocalContext.current
-    val activity = context as? Activity
-    val display = context.resources?.displayMetrics
-    val deviceWidth = display?.widthPixels
-    val deviceHeight = display?.heightPixels
-    fun px2dp(px: Int) =
-        px / ((context.resources.displayMetrics.densityDpi?.toFloat()) / DisplayMetrics.DENSITY_DEFAULT)
-
-    val listVerticalPadding = (px2dp(deviceWidth!!) - 200) / 2
-    // page를 이동하기 위한 상태 값
-    var currentCardId = rememberLazyListState()
-    val coroutineScope = rememberCoroutineScope()
-
+fun MyNayaCardList(context: Context) {
     val viewModel = viewModel<CardListViewModel>()
     viewModel.fetchCards()
     val cardList = viewModel.cardUris.value
-    val listSize = cardList.size
 
-    Column(
-        NayaCardContainerModifier,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        LazyRow(
-            NayaCardListModifier,
-            verticalAlignment = Alignment.CenterVertically,
-            contentPadding = PaddingValues(horizontal = listVerticalPadding.dp),
-            horizontalArrangement = Arrangement.spacedBy(32.dp),
-            state = currentCardId
-        ) {
-            items(cardList) { value ->
-                CardItem(value.uri, value.filename)
-            }
-
-            item() {
-                CardItemPlus()
-            }
-        }
-        Spacer(Modifier.height(16.dp))
-        Row {
-            if (listSize == 0) {
-                Text(text = "명함을 등록해 보세요", color = Color(0xFFCED3D6))
-            } else {
-                if (listSize > 2) {
-                    IconButton(onClick = {
-                        coroutineScope.launch { currentCardId.animateScrollToItem(0) }
-                    }) {
-                        Icon(
-                            Icons.Filled.ArrowLeft,
-                            "move to start"
-                        )
-                    }
-                    Spacer(Modifier.width(8.dp))
-                    IconButton(onClick = {
-                        coroutineScope.launch { currentCardId.animateScrollToItem(listSize) }
-                    }) {
-                        Icon(
-                            Icons.Outlined.Add,
-                            "plus"
-                        )
-                    }
-                    Spacer(Modifier.width(8.dp))
-                    IconButton(onClick = {
-                        coroutineScope.launch { currentCardId.animateScrollToItem(listSize - 1) }
-                    }) {
-                        Icon(
-                            Icons.Filled.ArrowRight,
-                            "move to end"
-                        )
-                    }
-                    Spacer(Modifier.width(8.dp))
-                } else {
-                    IconButton(onClick = {
-                        coroutineScope.launch { currentCardId.animateScrollToItem(listSize) }
-                    }) {
-                        Icon(
-                            Icons.Outlined.Add,
-                            "plus"
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
-
-
-@Composable
-@Preview
-fun HomeScreenPreview() {
-    MyNayaCardList()
+    CardHorizontalList(context, cardList)
 }
