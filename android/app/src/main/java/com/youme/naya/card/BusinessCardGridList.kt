b6c6cfef8fa8407f9a5f904e3c129926328788af
김성찬 <kimsc0714@naver.com>
2022-11-17 13:32:14 +0900

    ✨ feat: 카드 리스트가 비어있을 때 리스트 화면에 카드 생성 버튼 추가

--- android/app/src/main/java/com/youme/naya/card/BusinessCardGridList.kt
+++ android/app/src/main/java/com/youme/naya/card/BusinessCardGridList.kt
@@ -2,20 +2,21 @@ package com.youme.naya.card
 
 import android.app.Activity
 import android.content.Context
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.youme.naya.database.viewModel.CardViewModel
-import com.youme.naya.widgets.home.CardListViewModel
+import com.youme.naya.widgets.items.CardItemPlus
 import com.youme.naya.widgets.items.GalleryItem
 
 @Composable
@@ -31,16 +32,30 @@ fun BusinessCardGridList(
         cardViewModel.businessCardListInNaya.collectAsState().value
     }
 
-    LazyVerticalGrid(
-        modifier = Modifier
-            .padding(horizontal = 16.dp)
-            .fillMaxSize(),
-        columns = GridCells.Fixed(3),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(cardList) { card ->
-            GalleryItem(context as Activity, navController, bCard = card, enableShare = !isNuya)
+    if (cardList.isNotEmpty()) {
+        LazyVerticalGrid(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxSize(),
+            columns = GridCells.Fixed(3),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(cardList) { card ->
+                GalleryItem(context as Activity, navController, bCard = card, enableShare = !isNuya)
+            }
+        }
+    } else {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(bottom = 64.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CardItemPlus(isBCard = true, isNuya = isNuya)
+            Spacer(Modifier.height(16.dp))
+            Text(text = "명함을 등록해 보세요", color = Color(0xFFCED3D6))
         }
     }
 }
--- android/app/src/main/java/com/youme/naya/card/NayaCardGridList.kt
+++ android/app/src/main/java/com/youme/naya/card/NayaCardGridList.kt
@@ -2,17 +2,20 @@ package com.youme.naya.card
 
 import android.app.Activity
 import android.content.Context
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.youme.naya.widgets.home.CardListViewModel
+import com.youme.naya.widgets.items.CardItemPlus
 import com.youme.naya.widgets.items.GalleryItem
 
 @Composable
@@ -25,14 +28,33 @@ fun NayaCardGridList(context: Context, navController: NavHostController, isNuya:
     }
     val cardList = nayaCards.viewCards.value
 
-    LazyVerticalGrid(
-        modifier = Modifier.padding(horizontal = 16.dp),
-        columns = GridCells.Fixed(3),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(cardList) { card ->
-            GalleryItem(context as Activity, navController, nayaCard = card, enableShare = !isNuya)
+    if (cardList.isNotEmpty()) {
+        LazyVerticalGrid(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            columns = GridCells.Fixed(3),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(cardList) { card ->
+                GalleryItem(
+                    context as Activity,
+                    navController,
+                    nayaCard = card,
+                    enableShare = !isNuya
+                )
+            }
+        }
+    } else {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(bottom = 64.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CardItemPlus(isNuya = isNuya)
+            Spacer(Modifier.height(16.dp))
+            Text(text = "카드를 등록해 보세요", color = Color(0xFFCED3D6))
         }
     }
 }
