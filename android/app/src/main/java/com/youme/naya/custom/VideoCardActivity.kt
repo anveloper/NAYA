5d71d68189b480d842b0ade0f8619cea18865d8a
anveloper <hitedin@gmail.com>
2022-11-19 13:42:07 +0900

    feat: 비디오 컴포넌트 세팅

--- android/app/src/main/java/com/youme/naya/custom/VideoCardActivity.kt
+++ android/app/src/main/java/com/youme/naya/custom/VideoCardActivity.kt
@@ -2,38 +2,62 @@ package com.youme.naya.custom
 
 import android.app.Activity
 import android.graphics.Bitmap
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.util.Log
+import android.view.SurfaceView
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.IconButton
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBackIos
+import androidx.compose.material.icons.outlined.ArrowForwardIos
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.net.toUri
+import androidx.media3.common.C
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.datasource.DataSource
+import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
+import androidx.media3.ui.AspectRatioFrameLayout
+import androidx.media3.ui.PlayerView
 import com.youme.naya.BaseActivity
-import com.youme.naya.ui.theme.AndroidTheme
+import com.youme.naya.ui.theme.*
 
 class VideoCardActivity : BaseActivity(TransitionMode.HORIZON) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val activity = LocalContext.current as? Activity
+            val savedVideoAbsolutePath = intent.getStringExtra("savedVideoAbsolutePath")
+            val savedVideoUri = intent.getStringExtra("savedVideoUri")
+            if (savedVideoAbsolutePath != null) {
+                Log.i("savedVideoAbsolutePath", savedVideoAbsolutePath)
+            } else {
+                finish()
+            }
+            if (savedVideoUri != null) {
+                Log.i("savedVideoUri", savedVideoUri)
+            } else {
+                finish()
+            }
             AndroidTheme() {
-                VideoCardScreen(
+                VideoCardScreen(savedVideoUri!!.toUri(),
                     { bitmap ->
 //                        saveCardImage(baseContext, bitmap)
                         intent.putExtra("Custom Exit", 1)
@@ -52,45 +76,87 @@ class VideoCardActivity : BaseActivity(TransitionMode.HORIZON) {
 }
 
 @Composable
+@androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
 fun VideoCardScreen(
+    savedVideoUri: Uri,
     createNayaCardFile: (Bitmap) -> Unit,
     onFinish: () -> Unit
 ) {
     val context = LocalContext.current
-    val lifecycleOwner = LocalLifecycleOwner.current
-    val density = LocalDensity.current
-    val view = LocalView.current
 
-    val tmpBitmapList = remember {
-        mutableListOf<Bitmap>()
+    val exoPlayer = remember {
+        ExoPlayer.Builder(context)
+            .build()
+            .apply {
+                val defaultDataSourceFactory = DefaultDataSource.Factory(context)
+                val dataSourceFactory: DataSource.Factory = DefaultDataSource.Factory(
+                    context,
+                    defaultDataSourceFactory
+                )
+                val source = ProgressiveMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(MediaItem.fromUri(savedVideoUri))
+
+                setMediaSource(source)
+                prepare()
+            }
     }
 
-    val cameraX = CustomCameraX(context, lifecycleOwner)
-    Box(Modifier.fillMaxSize()) {
-        if (tmpBitmapList.size < 8) {
-            CustomCamera(context, cameraX) {
-                for (t in 1..8) {
-                    Handler(Looper.getMainLooper()).postDelayed({
-                        cameraX.capturePhoto() { bitmap ->
-                            tmpBitmapList.add(bitmap)
-                        }
-                    }, 1000 / 8)
-                }
+    exoPlayer.playWhenReady = true
+    exoPlayer.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
+    exoPlayer.repeatMode = Player.REPEAT_MODE_ONE
+
+    Box(Modifier.fillMaxSize(), Alignment.Center) {
+        Box(
+            Modifier
+                .width(300.dp)
+                .height(540.dp)
+                .background(NeutralGray)
+        ) {
+            val boxContext = LocalContext.current
+            val surfaceView = SurfaceView(boxContext)
+            val holder = surfaceView.holder
+            val player = MediaPlayer.create(boxContext, savedVideoUri)
+//            DisposableEffect(
+//                AndroidView(factory = {
+//                    PlayerView(boxContext).apply {
+//                        hideController()
+//                        useController = false
+//                        resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+//
+//                        player = exoPlayer
+//                        layoutParams = FrameLayout.LayoutParams(
+//                            ViewGroup.LayoutParams.MATCH_PARENT,
+//                            ViewGroup.LayoutParams.MATCH_PARENT
+//                        )
+//
+//                    }
+//                })
+//            ) {
+//                onDispose { exoPlayer.release() }
+//            }
+        }
+        Row(Modifier.fillMaxSize(), Arrangement.SpaceBetween, Alignment.Top) {
+            TextButton(
+                onClick = {
+                    onFinish()
+                }) {
+                Icon(
+                    Icons.Outlined.ArrowBackIos,
+                    "move to start",
+                    tint = NeutralLight
+                )
+                Text(text = "돌아가기", fontFamily = fonts, color = NeutralLight)
             }
-        } else {
-            // tmp
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(6),
-                contentPadding = PaddingValues(0.dp),
-                verticalArrangement = Arrangement.spacedBy(4.dp),
-                horizontalArrangement = Arrangement.spacedBy(4.dp)
-            ) {
-                items(tmpBitmapList) { sticker ->
-                    IconButton(onClick = {
-                    }) {
-                        Image(sticker.asImageBitmap(), null)
-                    }
-                }
+            TextButton(
+                onClick = {
+
+                }) {
+                Text(text = "완료", fontFamily = fonts, color = PrimaryBlue)
+                Icon(
+                    Icons.Outlined.ArrowForwardIos,
+                    "move to start",
+                    tint = PrimaryBlue
+                )
             }
         }
     }
